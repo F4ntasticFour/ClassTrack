7b85daf4d925af9f931cbef2ef8b0ad4a5660dc0
abdulrahman <s-abd.khlail@zewailciy.edu.eg>
2023-12-08 15:38:18 +0200

    test: testing routVlaues

--- Pages/CoursePage.cshtml.cs
+++ Pages/CoursePage.cshtml.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ClassTrack.Pages;
 
 public class CoursePage : PageModel
 {
+    [BindProperty (SupportsGet = true)]
+    public int CourseCode { get; set; }
+    public int StudentId { get; set; }
     public void OnGet()
     {
         
