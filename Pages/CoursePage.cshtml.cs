07c9ee42bceb26cc704fc45edea1575bde3e514a
marwanamr <maro99947@gmail.com>
2023-12-14 23:54:29 +0200

    feat: implement the course page

--- Pages/CoursePage.cshtml.cs
+++ Pages/CoursePage.cshtml.cs
@@ -1,15 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
-namespace ClassTrack.Pages;
-
-public class CoursePage : PageModel
+namespace ClassTrack.Pages
 {
-    [BindProperty (SupportsGet = true)]
-    public string CourseCode { get; set; }
-    public int StudentId { get; set; }
-    public void OnGet()
+    public class Session
     {
+        public int SessionID { get; set; }
         
+    }
+    public class CoursePage : PageModel
+    {
+        [BindProperty(SupportsGet = true)] 
+        public string CourseCode { get; set; }
+        public int StudentId { get; set; }
+        public List<Session> Sessions { get; set; }
+
+        public void OnGet()
+        {
+            string ConString = "Server=34.155.113.141,1433; Database=classtrack; User Id=sqlserver; Password=YUgMfE.H0^4A'zhS";
+            using (SqlConnection con = new SqlConnection(ConString))
+            {
+                string querystring1 = "SELECT cs.session_id FROM class_session cs JOIN course_section csct ON csct.section_id = cs.section_id JOIN enroll e ON e.course_code = csct.course_code WHERE e.student_id = @StudentID AND e.course_code = @CourseCode";
+                try
+                {
+                    con.Open();
+                    SqlCommand read = new SqlCommand(querystring1, con);
+                    read.Parameters.AddWithValue("@CourseCode", CourseCode);
+                    read.Parameters.AddWithValue("@StudentID", StudentId);
+                    SqlDataReader reader = read.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var session = new Session
+                        {
+                            SessionID = (int)reader[0]
+                        };
+                        Sessions.Add(session);
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    // Log the exception for debugging and monitoring
+                }
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/AttendanceSubmission", new { CourseCode = CourseCode, StudentId = StudentId });
+        }
     }
 }
