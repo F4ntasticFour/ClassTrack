bc34c50c879f77b6b4df77aa4542e9e3b839b630
ustafaa <s-mostafa.abdalelah@zewailcity.edu.eg>
2023-12-14 19:54:08 +0200

    ankalys

--- Pages/StudentPage.cshtml.cs
+++ Pages/StudentPage.cshtml.cs
@@ -23,7 +23,7 @@ namespace ClassTrack.Pages
 
         public void OnGet()
         {
-            string ConString = "Server=34.155.113.141,1433; Database=classtrack; User Id=sqlserver; Password=YUgMfE.H0^4A'zhS";
+            string ConString = "Server=localhost; Database=master; User Id=sa; Password=reallyStrongPwd123";
 
             using (SqlConnection con = new SqlConnection(ConString))
             {
